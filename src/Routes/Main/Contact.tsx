6948d5fc970d146464d793df3b508874cc8c9718
vanessa <jumpingboys@naver.com>
2023-07-11 15:41:50 +0900

    [CONTACT] form element 동적 생성되는 이벤트 리팩토링

--- src/Routes/Main/Contact.tsx
+++ src/Routes/Main/Contact.tsx
@@ -16,30 +16,22 @@ function Contact() {
 
   // emailJs에서 호환되는 formElement를 동적으로 생성해주는 이벤트
   const FormElementMaker = (data: any) => {
-    const test = document.createElement('form');
-    test.setAttribute('id', 'form');
-    const hiddenField = document.createElement('input');
-    hiddenField.setAttribute('name', 'user_name');
-    hiddenField.setAttribute('value', data.user_name);
-    hiddenField.setAttribute('type', 'text');
-    const hiddenField2 = document.createElement('input');
-    hiddenField2.setAttribute('name', 'user_email');
-    hiddenField2.setAttribute('value', data.user_email);
-    hiddenField2.setAttribute('type', 'text');
-    const hiddenField3 = document.createElement('input');
-    hiddenField3.setAttribute('name', 'message');
-    hiddenField3.setAttribute('value', data.message);
-    hiddenField3.setAttribute('type', 'text');
-    test.appendChild(hiddenField);
-    test.appendChild(hiddenField2);
-    test.appendChild(hiddenField3);
-    return test;
+    const form = document.createElement('form');
+
+    Object.keys(data).forEach((key: string) => {
+      const hiddenField = document.createElement('input');
+      hiddenField.setAttribute('name', key);
+      hiddenField.setAttribute('value', data[key]);
+      hiddenField.setAttribute('type', 'text');
+      form.appendChild(hiddenField);
+    });
+
+    return form;
   };
 
   // 메일 보내기 이벤트
   const sendEmail: SubmitHandler<IForm> = async (data) => {
     if (Object.keys(errors).length > 0 || !isValid) return;
-
     try {
       const result = await emailjs.sendForm('service-vanessa', 'template_13k5i08', FormElementMaker(data), 'fm7oF7rpZ--KdiXEG');
     } catch (err) {
